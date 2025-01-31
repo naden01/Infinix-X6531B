39c749a1620ff8c4fe844cb1eccd321ec9ecfaa0
nazephyrus <126683488+naden01@users.noreply.github.com>
2025-01-31 05:34:26 -0800

    little changes

--- twrp_X6531B.mk
+++ twrp_X6531B.mk
@@ -10,20 +10,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from Infinix-X6531B device
 $(call inherit-product, device/infinix/Infinix-X6531B/device.mk)
 
 PRODUCT_DEVICE := Infinix-X6531B
-PRODUCT_NAME := omni_Infinix-X6531B
+PRODUCT_NAME := twrp_X6531B
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X6531B
 PRODUCT_MANUFACTURER := infinix
 
 PRODUCT_GMS_CLIENTID_BASE := android-infinix
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vnd_x6531_v631-user 12 SP1A.210812.016 694928 release-keys"
-
-BUILD_FINGERPRINT := Infinix/X6531B-OP/Infinix-X6531B:12/SP1A.210812.016/240904V384:user/release-keys
