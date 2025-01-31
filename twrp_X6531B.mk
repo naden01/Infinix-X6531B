64b7a1d17f955b03cff1fdab560c4f63ab01053e
nazephyrus <126683488+naden01@users.noreply.github.com>
2025-01-31 05:34:38 -0800

    iforgot

--- twrp_X6531B.mk
+++ twrp_X6531B.mk
@@ -13,7 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from Infinix-X6531B device
-$(call inherit-product, device/infinix/Infinix-X6531B/device.mk)
+$(call inherit-product, device/infinix/X6531B/device.mk)
 
 PRODUCT_DEVICE := Infinix-X6531B
 PRODUCT_NAME := twrp_X6531B
